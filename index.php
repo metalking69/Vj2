89a39c7ce7005c92e7dc9e895d3cd619844aff67
metalking69 <43329149+metalking69@users.noreply.github.com>
2019-11-25 19:32:41 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,262 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Welcome Home</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Plugin CSS -->
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/creative.min.css" rel="stylesheet">
+
+    <style>
+        video
+        {
+            width: 100%;
+            max-width: 1080px;
+        }
+
+        @media only screen and (max-width: 800px) 
+        {
+            /*video
+            {
+                max-width: 480px;
+            }*/
+        }
+    </style>
+      
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">Welcome Home</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#portfolio">Media</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#about">Detalles</a>
+            </li>
+            
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact">Desarrollador</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <header class="masthead text-center text-white d-flex">
+      <div class="container my-auto">
+        <div class="row">
+          <div class="col-lg-10 mx-auto">
+            <img class="img-fluid" src="img/portfolio/thumbnails/shoot.png" alt="">       
+              <hr class="invisible">
+              <h1 class="text-uppercase">         
+              <strong class="text-dark">Sobrevive en</strong>
+                  <br>
+                <img class="img-fluid" src="img/steam.png" width="100px;">
+                 
+            </h1>
+            <hr>
+          </div>
+          <div class="col-lg-8 mx-auto">
+         
+            <a class="btn btn-primary btn-xl js-scroll-trigger" href="#portfolio">Entra... si te atreves</a>
+          </div>
+        </div>
+      </div>
+    </header>
+      
+      <section class="p-0 bg-dark" id="portfolio">
+      <div class="container-fluid p-0">
+        <div class="row no-gutters popup-gallery">
+         <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/fullsize/4.jpg">
+              <img class="img-fluid" style="max-height: 300px;" src="img/portfolio/thumbnails/4.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/fullsize/5.jpg">
+              <img class="img-fluid" style="max-height: 300px;" src="img/portfolio/thumbnails/5.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/fullsize/6.jpg">
+              <img class="img-fluid" style="max-height: 300px;" src="img/portfolio/thumbnails/6.jpg" alt="">
+              <div class="portfolio-box-caption">
+
+              </div>
+            </a>
+          </div>
+        </div>
+          <div class="row no-gutters popup-gallery">
+              <div class="col text-center">
+                  <video width="auto" height="auto" controls >
+<source src="img/Comercial1.mp4" type="video/mp4" media="screen and (min-width:320px)"></video>  
+              </div>
+          </div>
+          <div class="row no-gutters popup-gallery">
+              <div class="col text-center">
+                  <video width="auto" height="auto" controls >
+<source src="img/Comercial2.mp4" type="video/mp4" media="screen and (min-width:320px)"></video>  
+              </div>
+          </div>
+      </div>
+    </section>
+
+
+    <section class="bg-primary" id="about">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto text-center">
+            <h2 class="section-heading text-white">Has regresado a tu pueblo natal después de mucho tiempo... Pero algo anda mal.</h2>
+            <br>
+            <h5 class="text-faded mb-4">Intenta salvar al pueblo del levantamiento de los muertos, pero recuerda... ¡No mueras!</h5>     
+              <hr class="light my-4">
+              <p class="text-faded mb-1">¿Cantidad de jugadores y modos de juego?</p>
+              <p class="text-faded mb-4">- 1 jugador y 1 modo de juego, sobrevivir la gran horda de zombies.</p>
+              
+              <p class="text-faded mb-1">¿Que me puede matar?</p>
+              <p class="text-faded mb-4">- Todos los zombies están capacitados y son más que capaces de comer tus cerebros.</p>
+              
+              <p class="text-faded mb-1">¿Como consigo un mayor puntaje?</p>
+              <p class="text-faded mb-4">- Éste juego no es acerca de puntos, es acerca de sobrevivir, y si quieres lograrlo, tendrás que ser más rápido y más listo que los zombies.</p>              
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="services">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <i class="fas fa-4x fa-gem text-primary mb-3 sr-icon-1"></i>
+              <h3 class="mb-3">Recompenza</h3>
+              <p class="text-muted mb-0">Salvar al pueblo de la total aniquilación en un escenario postapocalíptico.</p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <i class="fas fa-4x fa-paper-plane text-primary mb-3 sr-icon-2"></i>
+              <h3 class="mb-3">Explora</h3>
+              <p class="text-muted mb-0">Descubre los misterios del cementario del pueblo de stonewall.</p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <i class="fas fa-4x fa-code text-primary mb-3 sr-icon-3"></i>
+              <h3 class="mb-3">Completamente actualizado</h3>
+              <p class="text-muted mb-0">Creado con Unreal Engine 4, el juego promete una experiencia óptima y sin errores.</p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <i class="fas fa-4x fa-heart text-primary mb-3 sr-icon-4"></i>
+              <h3 class="mb-3">Mantente vivo</h3>
+              <p class="text-muted mb-0">Los zombies que rondan alrededor no te dejarán ni un momento para respirar.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    
+
+    <section id="contact">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto text-center">
+              <img src="img/LUPUS GAMES_thumbnail.png" style="max-width: 480px;">
+            <hr class="my-4">
+            <h5 class="mb-5">En Lupus Games valoramos tus comentarios y sugerencias!</h5>
+            <h5 class="mb-5">Desadrrollado con amor de parte de:</h5>
+          </div>
+        </div>
+          <br>
+        <div class="row">
+          <div class="col-lg-4 ml-auto text-center">
+            <p>Carlos Alberto López Solis</p>
+              <a href="https://www.facebook.com/carlos.a.lopez.9216778">
+              <img src="img/facebook.png" width="50px" class="mb-3">
+              </a>
+          </div>
+          <div class="col-lg-4 mr-auto text-center">
+            "He estado en el mundo de la programación desde hace 7 años y los videojuegos son una de mis pasiones, tanto jugarlos como desarrollarlos."
+          </div>
+        </div>
+          <br><br>
+          <div class="row">
+          <div class="col-lg-4 ml-auto text-center">
+            <p>Sergio Fernando Vázquez Félix</p>
+              <a href="https://www.twitter.com/rhoric_">
+              <img src="img/tuater.png" width="50px" class="mb-3">
+              </a>
+          </div>
+          <div class="col-lg-4 mr-auto text-center">
+              "Todas las deciciones que he tomado en mi vida me han llevado hasta donde estoy. Y no me arrepiento."
+          </div>
+        </div>
+          <br><br>
+          <div class="row">
+          <div class="col-lg-4 ml-auto text-center">
+            <p>Angel Axel Garza Flores</p>
+              <a href="https://twitter.com/pewterrock096">
+              <img src="img/tuater.png" width="50px" class="mb-3">
+              </a>
+          </div>
+          <div class="col-lg-4 mr-auto text-center">
+            "Mi amor por los videojuegos como medio con la capacidad de hacerte vivir una experiencia me ha llevado a querer formar parte de este como programador."
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/creative.min.js"></script>
+
+  </body>
+
+
+</html>
